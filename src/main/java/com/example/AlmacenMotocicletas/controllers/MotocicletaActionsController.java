3474d933f8be5ba0c47558f5c14208219161bd88
Cristhian <yurday93@hotmail.com>
2021-07-16 11:59:34 -0500

    Adición del CrossOrigin para visualización de los datos en la app

--- src/main/java/com/example/AlmacenMotocicletas/controllers/MotocicletaActionsController.java
+++ src/main/java/com/example/AlmacenMotocicletas/controllers/MotocicletaActionsController.java
@@ -5,11 +5,9 @@ import com.example.AlmacenMotocicletas.services.MotocicletaActionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("https://catalogomotos.web.app")
 @RestController
 @RequestMapping("/motocicletas")
 public class MotocicletaActionsController {
